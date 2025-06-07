416202aa154c5d07ae3778ff5672faba08cf3476
ChairulNew <chairularul010805@gmail.com>
2025-06-07 14:20:22 +0700

    sesuaikan

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./index.css";
 import Navbar from "../src/Pages/Navbar/Navbar";
 import Header from "./Pages/Header/Header";
+import SectionMain from "./Pages/Section/Section";
 function App() {
   return (
     <>
       <div>
         <Navbar />
         <Header />
+        <SectionMain />
       </div>
     </>
   );
---
+++ src/Pages/Section/Section.jsx
@@ -0,0 +1,10 @@
+import "./Section.css";
+const SectionMain = () => {
+  return (
+    <section id="section-main">
+      <div></div>
+    </section>
+  );
+};
+
+export default SectionMain;
