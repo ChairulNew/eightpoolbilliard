9c47d9daf6cf080b5ea31d27e8d0d17352c05531
ChairulNew <chairularul010805@gmail.com>
2025-07-17 19:32:32 +0700

    fix navbar

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -1,29 +1,18 @@
 import VideoHeader from "../../assets/video-header.mp4";
 import "./Header.css";
 import gap from "../../assets/gap.png";
-// import gapBottom from "../../assets/gap-bottom.png";
-// import "../Section/Section1";
+import Navbar from "../Navbar/Navbar"; // Pastikan path-nya benar
 
 const Header = () => {
-  const handleScrollToSection = () => {
-    const section = document.getElementById("section1");
-    if (section) {
-      section.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
   return (
     <section id="header">
       <div className="header-wrap">
+        <Navbar /> {/* Navbar ditambahkan di sini */}
         <div className="slogan-text-wrap" style={{ marginTop: -100 }}>
           <h1 className="title-heading">8POOL</h1>
-
           <h1 className="title-heading" style={{ color: "yellow" }}>
             BILLIARD
           </h1>
-          {/* <button className="btn_title" onClick={handleScrollToSection}>
-            Click for more information
-          </button> */}
         </div>
         <video
           src={VideoHeader}
@@ -37,6 +26,7 @@ const Header = () => {
           <p className="header-text-bottom">SINCE 2022</p>
         </div>
       </div>
+
       <div className="gap-wrapper">
         <img src={gap} alt="gap" className="gap-img" />
       </div>
